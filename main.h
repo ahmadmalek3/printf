cb3a282339fbce20eeb028fafcd4717579343136
ahmadmalek3 <ahmadmale3@outlook.com>
2023-10-17 20:10:04 +0300

    commit

---
+++ main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+int _printf(const char *format, ...);
+int _print_integer(int n);
+int print_chara(char c);
+
+#endif /* MAIN_H */
