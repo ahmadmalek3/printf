33c2c9806fde03d72cac4b3e4e3878870bd5ddfa
esraa <esraad98d@gmail.com>
2023-10-17 09:43:38 -0700

    commit

---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+int _printf(const char *format, ...);
+int _print_integer(int n);
+int print_chara(char c);
+int print_pero(void);
+
+#endif /* MAIN_H */
