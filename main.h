b6af0a4ec36ae1e2951fee42604020f10bac5475
ahmadmalek3 <ahmadmale3@outlook.com>
2023-10-18 01:41:40 +0300

    main.h

--- main.h
+++ main.h
@@ -9,11 +9,5 @@
 int _printf(const char *format, ...);
 int _print_integer(int n);
 int print_chara(char c);
-<<<<<<< HEAD
-=======
-int print_int(va_list args);
-int print_dec(va_list args);
-int print_bin(va_list valy);
->>>>>>> 5f46ff13a385eecd27470387ca62f2581521686a
 
 #endif /* MAIN_H */
